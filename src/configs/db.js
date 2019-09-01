fcf81aa4e8c490b3c575ee8d4ea2a57a23ea265a
Poko Groot <PokoGroot@users.noreply.github.com>
2019-09-01 21:46:31 +0700

    setting db

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    host: remotemysql.com,
-    user: b2Vvt6djbP,
-    password: Feh2gSe1qP,
-    database: b2Vvt6djbP
+    host: "remotemysql.com",
+    user: "b2Vvt6djbP",
+    password: "Feh2gSe1qP",
+    database: "b2Vvt6djbP"
 })
 
 module.exports = conn
